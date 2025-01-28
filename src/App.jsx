2452d466b9270e95cf3472d26ef18e6264c1fcb3
cesarDanielRodrigues <cesar.daniel.rodrigues@gmail.com>
2025-01-28 07:44:30 -0300

    Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-function App() {
-  return <h1>Hello World</h1>
-}
+import { Post } from "./Post"
 
-export default App
+export function App() {
+  return (
+    <div>
+      <Post
+        author="César Daniel"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, quidem iure repellendus nobis quod deleniti facilis impedit? Expedita maiores fugiat culpa provident? Iste, iusto eius voluptatibus saepe expedita ullam laboriosam."
+      />
+    </div>
+  )
+}
