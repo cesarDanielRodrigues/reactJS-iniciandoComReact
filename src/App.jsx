502e4e5bbb8d3a144069dd4a1dc321bf2c12c7cf
cesarDanielRodrigues <cesar.daniel.rodrigues@gmail.com>
2025-02-14 11:29:55 -0300

    Componente: sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
+
+import styles from "./App.module.css"
+
+import "./global.css"
+import { Sidebar } from "./components/sidebar"
 
 export function App() {
   return (
     <div>
-      <Post
-        author="César Daniel"
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, quidem iure repellendus nobis quod deleniti facilis impedit? Expedita maiores fugiat culpa provident? Iste, iusto eius voluptatibus saepe expedita ullam laboriosam."
-      />
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post
+            author="César Daniel"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, quidem iure repellendus nobis quod deleniti facilis impedit? Expedita maiores fugiat culpa provident? Iste, iusto eius voluptatibus saepe expedita ullam laboriosam."
+          />
+        </main>
+      </div>
     </div>
   )
 }
