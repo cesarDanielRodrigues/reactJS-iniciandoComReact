b3470e37b32411b865b299437f3a346c50868efc
cesarDanielRodrigues <cesar.daniel.rodrigues@gmail.com>
2025-02-14 14:31:12 -0300

    Finalizando o sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,8 @@
 import styles from "./Sidebar.module.css"
 
+import {PencilLine} from "@phosphor-icons/react"
+
+
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
@@ -8,11 +11,18 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
+        <img 
+            className={styles.avatar} 
+            src="https://github.com/cesarDanielRodrigues.png"
+        />
         <strong>profile</strong>
         <span>subtitle</span>
       </div>
       <footer>
-        <a href="http://">Editar seu perfil</a>
+        <a href="http://">
+          <PencilLine size={15}/>
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   )
